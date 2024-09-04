d7e88e0331a3a25210e9541ad28ef125bf9862a7
ranji <ranji@RANJITHA>
2024-09-04 14:36:56 +0530

    This my project

---
+++ C2TC_Ranjitha/src/com/tns/day2/FourthProgram.java
@@ -0,0 +1,30 @@
+package com.tns.day2;
+
+public class FourthProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=10;i=i+2)
+		{
+			System.out.println(i);
+		}
+         
+		
+		 
+		int j=0;
+         while(j<=6)
+         {
+        	 System.out.println("hello , welcome to java");
+             j++;
+         }
+             
+             int s=0;
+             do {
+                   System.out.println("keep learning");
+                   s++;
+                   
+             }while(s<=4);
+
+	}
+
+}
---
+++ C2TC_Ranjitha/src/com/tns/day2/SecondProgram.java
@@ -0,0 +1,18 @@
+package com.tns.day2;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
+
+
